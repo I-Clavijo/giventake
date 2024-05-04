dbd684ac71d9c7e4902768fa81dd6145cb6e8e99
Ivan Clavijo <ivan.clavijo2@gmail.com>
2024-05-04 03:59:31 +0300

    fix

--- src/server/config/allowedOrigins.js
+++ src/server/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://adaa-46-117-238-113.ngrok-free.app',
+    'https://adaa-46-117-238-113.ngrok-free.app'
 ];
 
 export default allowedOrigins;
