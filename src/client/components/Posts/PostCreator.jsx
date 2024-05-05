86324a6f8ea8845dc6ca346a4f49deea7a3135b9
oribenez <oriben467@gmail.com>
2024-05-05 16:10:53 +0300

    small tweaks still need work to connect it to server

--- src/client/components/Posts/PostCreator.jsx
+++ src/client/components/Posts/PostCreator.jsx
@@ -1,5 +1,5 @@
 import styles from "./PostCreator.module.scss";
-import { Textarea, Button, Datepicker, FileInput, Label, TextInput } from "flowbite-react";
+import { Textarea, Button, Datepicker, FileInput, Label, TextInput, Checkbox, Card } from "flowbite-react";
 import { useForm } from 'react-hook-form';
 import { date, z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -61,136 +61,126 @@ const PostCreator = () => {
   const dateValue = getValues('date');
   const endDateValue = getValues('endDate');
 
-  return (
-    <div className={styles.postCreator}>
-      <header className={styles.header}>
-        <h2 className={styles.title}>Create new post</h2>
-      </header>
+  return <div className={styles.postCreator}>
+    <h4>Let's Start!</h4>
+    <p>Ready to tap into the collective wisdom of our amazing community?<br /> Don't hesitate to create a new post if you find yourself in need of assistance.<br /> <i>Just as you've helped others, it's now your turn to ask for support.</i><br /> Together, we can overcome any challenge. Describe your situation, and let the magic of our community work its wonders! <br />  </p>
 
-      <section className="container mx-auto px-4 py-8">
-        <form onSubmit={handleSubmit(createPost)}>
+    <h3 className={styles.textBoldOut}>Together, we're unstoppable!</h3>
 
-          <div className="mb-4">
-            <label className={styles.label} htmlFor="category">Choose category:</label>
-            <select {...register('category')} id="category" className="form-select px- py-1.7 w-full rounded-md">
-              <option value="">Category</option>
-              {categories.map((Category) => (
-                <option key={Category} value={Category}>
-                  {Category}
-                </option>
-              ))}
-            </select>
-          </div>
+    <Card>
+      <h4>Create new post</h4>
+      <form onSubmit={handleSubmit(createPost)}>
+        <div className="mb-2 mt-4">
+          <Label className={styles.label} htmlFor="category">Choose an appropriate category *</Label>
+          <select {...register('category')} id="category" className="form-select px- py-1.7 w-full rounded-md">
+            <option value="">Category</option>
+            {categories.map((Category) => (
+              <option key={Category} value={Category}>
+                {Category}
+              </option>
+            ))}
+          </select>
+        </div>
 
-          <div className={styles.helpDates}>
-            <div className="flex mb-4">  <label className={styles.label}>Date:</label>
-              <span className={styles.startDate}>
-                <Datepicker value={dateValue?.toDateString()} onSelectedDateChanged={(value) => setValue('date', value)} showClearButton={false} showTodayButton={false} id="date" minDate={new Date()} {...register('date')} />
+        <div className={styles.helpDates}>
+          <Label className={styles.label}>When do you need your help?</Label>
+          <div className="flex mb-2">
+            <span>
+              <Datepicker value={watch('startDate').toDateString()} onSelectedDateChanged={(value) => { setValue('startDate', value); setValue('endDate', value) }} showClearButton={false} showTodayButton={false} id="startDate" minDate={new Date()} {...register('startDate')} />
+            </span>
+
+            {!isAllDay && (
+              <span className={styles.startTime}>
+                <TextInput placeholder='Start time (24h)' {...register('startTime')} maxLength={5} />
               </span>
+            )}
 
-              {!isAllDay && (
-                <>
-                  <label className={styles.label}>From:</label>
-                  <span className={styles.startTime}>
-                    <TextInput placeholder='Start time (24h)' {...register('startTime')} />
-                  </span>
-                </>
-              )}
-
-              {!isAllDay && !isMultipleDay && (
-                <>
-                  <label className={styles.label}>To:</label>
-                  <span className={styles.endTime}>
-                    <TextInput placeholder='End time (24h)' {...register('endTime')} />
-                  </span>
-                </>
-              )}
-            </div>
-
-            <div className="mb-4 flex items-center">  <label>
-              <input type="checkbox" checked={isAllDay} onChange={(e) => setIsAllDay(e.target.checked)} />
-              All day
-            </label>
-
-              <label className="ml-4">  <input type="checkbox" checked={isMultipleDay} onChange={(e) => setIsMultipleDay(e.target.checked)} />
-                Multiple days  
-              </label>
-            </div>
-
+            {!isAllDay && isMultipleDay && (
+              <>
+                <label className={styles.label}>to</label>
+                <span className={styles.endTime}>
+                  <TextInput placeholder='End time (24h)' {...register('endTime')} maxLength={5} />
+                </span>
+              </>
+            )}
             {isMultipleDay && (
-              <div className="mb-4">
-                <label className={styles.label}>End date:</label>
-                <span className={styles.endDate}>
-                  <Datepicker value={watch('endDate').toDateString()} showClearButton={false} showTodayButton={false} onSelectedDateChanged={(value) => setValue('endDate', value)} id="endDate" minDate={watch('startDate')} {...register('endDate')} />
-                </span>
-                <label className={styles.label}>To:</label>
-                  <span className={styles.endTime}>
-                    <TextInput placeholder='End time (24h)' {...register('endTime')} />
-                  </span>
-              </div>
+              <span>
+                <Datepicker value={watch('endDate').toDateString()} showClearButton={false} showTodayButton={false} onSelectedDateChanged={(value) => setValue('endDate', value)} id="endDate" minDate={watch('startDate')} {...register('endDate')} />
+              </span>
             )}
           </div>
 
-
-
-
-          <div className={styles.voluPlace}>
-            <label className={styles.label} htmlFor="city">Location:</label>
-            {!isRemote && (
-              <div>  {/* Wrap the following elements */}
-                <span>
-                  <TextInput placeholder='Address' {...register('address')} />
-                </span>
-                <span>
-                  <TextInput placeholder='City' {...register('city')} />
-                </span>
-              </div>
-            )}
-            <label>
-              <input type="checkbox" checked={isRemote} onChange={(e) => setRemote(e.target.checked)} />
-              Remote Help
-            </label>
+          <div className="flex">
+            <div className="flex gap-2 items-center">
+              <Checkbox id="allDay" checked={isAllDay} onChange={(e) => setIsAllDay(e.target.checked)} />
+              <Label htmlFor="allDay">All day</Label>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Checkbox id="multipleDays" checked={isMultipleDay} onChange={(e) => setIsMultipleDay(e.target.checked)} />
+              <Label htmlFor="multipleDays">Multiple days</Label>
+            </div>
           </div>
-
-          <div className="mb-4">
-            <label className={styles.label}>Picture of your post:</label>
-            <div>
-              <div className="flex w-full items-center justify-center">
-                <Label htmlFor="dropzone-file" className="flex h-32 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600" >
-                  <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                    {!watch('img')?.length ?
-                      <>
-                        <svg className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16" > <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" /> </svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                          <span className="font-semibold">Click to upload</span> or drag and drop
-                        </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                      </> :
-                      <span className="font-semibold">File loaded</span>
-                    }
+        </div>
 
 
-                  </div>
-                  <FileInput id="dropzone-file" className="hidden" accept="image/*" {...register('img')} />
-                </Label>
-              </div>
+        <div className="mb-2">
+          <div className="flex gap-2 mb-2">
+            <Label className={styles.label} htmlFor="city">Where do you need your help? </Label>
+            <div className={styles.remoteHelp}>
+              <Checkbox id="remoteHelp" checked={isRemote} onChange={(e) => setRemote(e.target.checked)} />
+              <Label htmlFor="remoteHelp">Remote Help</Label>
             </div>
           </div>
 
-          <div className="mb-4">
-            <label className={styles.label} htmlFor="description"> Description: </label>
-            <div className="mb-2 block"> <Label htmlFor="description" /> </div>
-            <Textarea id="description" placeholder="Write here" rows={5} {...register('description')} />
-          </div>
+          {!isRemote && (
+            <div className="flex items-center gap-2">
+              {/* <Label htmlFor="address">Address:</Label> */}
+              <TextInput id="address" placeholder='Address' {...register('address')} />
+              {/* <Label htmlFor="city">City:</Label> */}
+              <TextInput id="city" placeholder='City' {...register('city')} />
+            </div>
 
-          <div className={styles.submitButton}>
-            <Button type='submit' disabled={isSubmitting} className='button'>Create post</Button>
-          </div>
-        </form>
-      </section>
-    </div>
+          )}
+        </div>
 
-  );
+        <div className="mb-4">
+          <Label className={styles.label}>Picture of your post (optional)</Label>
+          <div>
+            <div className="flex w-full items-center justify-center">
+              <Label htmlFor="dropzone-file" className="flex h-32 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600" >
+                <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                  {!watch('img')?.length ?
+                    <>
+                      <svg className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16" > <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" /> </svg>
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span> or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </> :
+                    <span className="font-semibold">File loaded</span>
+                  }
+
+
+                </div>
+                <FileInput id="dropzone-file" className="hidden" accept="image/*" {...register('img')} />
+              </Label>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <Label className={styles.label} htmlFor="description"> Description </Label>
+          <div className="mb-2 block"> <Label htmlFor="description" /> </div>
+          <Textarea id="description" placeholder="Write here" rows={5} {...register('description')} />
+        </div>
+
+        <div className={styles.submitButton}>
+          <Button type='submit' disabled={isSubmitting} className='button'>Create post</Button>
+        </div>
+      </form>
+
+    </Card>
+  </div>
 };
 
 export default PostCreator;
