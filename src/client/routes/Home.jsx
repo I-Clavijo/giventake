a2fc4f19e9f6e9ed90a179725b62b1578a5ccf55
oribenez <oriben467@gmail.com>
2024-05-10 22:15:29 +0300

    fix post action bug

--- src/client/routes/Home.jsx
+++ src/client/routes/Home.jsx
@@ -1,21 +1,23 @@
 import styles from "./Home.module.scss";
-import Feed, { showAs } from "../components/Posts/Feed.jsx";
+import Feed from "../components/Posts/Feed.jsx";
 import { usePosts } from "../api/posts/usePosts.jsx";
 import { useUser } from "../api/users/useUser.jsx";
 import { Spinner } from "flowbite-react";
-import { useState } from "react";
 import WelcomeModal from "../components/WelcomeModal.jsx";
-import FeedFilters from "../components/Posts/FeedFilters.jsx";
+import { usePostAction } from "../api/posts/usePostAction.jsx";
 
 export default function Home() {
   const { data: user, isLoading: isLoadingUser, isError: isErrorUser } = useUser();
-  const { data: posts, isLoading: isLoadingPosts } = usePosts({ filters: { onlyPeopleIFollow: 1 } });
+  const filters = { onlyPeopleIFollow: 1 };
+
+  const { mutate: postAction } = usePostAction({ filters });
+  const { data: posts, isLoading: isLoadingPosts } = usePosts({ filters });
 
   return <>
     {user?.flags?.hideWelcomeModal === false && <WelcomeModal />}
 
     {posts && !isLoadingPosts
-      ? <Feed posts={posts} />
+      ? <Feed {...{ posts }} onPostAction={postAction} />
       : <Spinner />
     }
   </>;
