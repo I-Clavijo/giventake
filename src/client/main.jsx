2c35e8e0c7a301e23c777902ee7e018dd9e07749
Ivan Clavijo <ivan.clavijo2@gmail.com>
2024-05-11 18:34:00 +0300

    fix

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -21,7 +21,6 @@ import Admin from "./routes/Admin";
 import Unauthorized from "./components/Auth/Unauthorized";
 import SavedPosts from "./routes/SavedPosts";
 import Create from "./routes/Create";
-import VerifyEmail from "./routes/VerifyEmail";
 
 const ROLES = {
 	'User': 2001,
@@ -52,7 +51,6 @@ const router = createBrowserRouter([
 							{ path: "/profile", element: <Profile isMyProfile={true} /> },
 							{ path: "/create", element: <Create />},
 							{ path: "/messages", element: <Messages /> },
-							{ path: "/verify-email", element: <VerifyEmail /> },
 						] 
 					},
 					{ 
