b334bdc65ddcabc22e97c62f96456ca370c43e95
oribenez <oriben467@gmail.com>
2024-05-29 15:23:43 +0300

    update main for deployment

--- src/server/config/allowedOrigins.js
+++ src/server/config/allowedOrigins.js
@@ -1,10 +1,11 @@
-import { VITE_FORWARDED_ADDRESS } from "../config.js";
+import { VITE_FORWARDED_ADDRESS } from '../config.js'
 
 const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    VITE_FORWARDED_ADDRESS
-];
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+  'http://localhost:3000',
+  'https://giventake.onrender.com',
+  VITE_FORWARDED_ADDRESS
+]
 
-export default allowedOrigins;
+export default allowedOrigins
